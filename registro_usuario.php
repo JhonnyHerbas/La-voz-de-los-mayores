4d26c09c4eb661bf087ec4670697ad7aa1e47d0d
Darwin <darwingarcia2130@gmail.com>
2022-04-28 15:27:25 -0400

    contraseña segura

--- registro_usuario.php
+++ registro_usuario.php
@@ -8,53 +8,63 @@
     $confirmar_contrasena = $_POST['contrasena2'];
 
     if(strcmp($contrasena, $confirmar_contrasena) ==0){
-        $query = "INSERT INTO usuarios(NOMBRE_U, CONTRASENIA_U, CORREO_U) 
-            VALUES('$nombre_usuario','$contrasena', '$correo')";
-       
-
-        //verificar que el correo no se repita
-        $query_correo="SELECT * FROM `usuarios` WHERE CORREO_U='$correo' ";
-        $verificar_correo = mysqli_query($connection,$query_correo);
-        $row_cont = $verificar_correo->num_rows;
-        if($row_cont> 0){
-            echo '
-                <script>
-                    alert("Este correo ya fue registrado, intente con otro correo");
-                    window.location = "registro.html";
-                </script>
-            ';
-            exit();
-        }
-        //verificar que el nombre no se repita
-        $query_usuario="SELECT * FROM `usuarios` WHERE NOMBRE_U='$nombre_usuario' ";
-        $verificar_usuario = mysqli_query($connection,$query_usuario);
-        $row_cont_U = $verificar_usuario->num_rows;
-        if($row_cont_U> 0){
-            echo '
-                <script>
-                    alert("Este usuario ya fue registrado, intente con otro usuario");
-                    window.location = "registro.html";
-                </script>
-            ';
-            exit();
-        }
+        if(validar_clave($contrasena)){
+                    
+                $query = "INSERT INTO usuarios(NOMBRE_U, CONTRASENIA_U, CORREO_U) 
+                VALUES('$nombre_usuario','$contrasena', '$correo')";
         
-        $ejecutar = mysqli_query($connection,$query);
 
-        if($ejecutar){
-            echo '
-                <script>
-                    alert("Usuario creado exitosamente");
-                    window.location = "index.html";
-                </script>
-            ';
+            //verificar que el correo no se repita
+            $query_correo="SELECT * FROM `usuarios` WHERE CORREO_U='$correo' ";
+            $verificar_correo = mysqli_query($connection,$query_correo);
+            $row_cont = $verificar_correo->num_rows;
+            if($row_cont> 0){
+                echo '
+                    <script>
+                        alert("Este correo ya fue registrado, intente con otro correo");
+                        window.location = "registro.html";
+                    </script>
+                ';
+                exit();
+            }
+            //verificar que el nombre no se repita
+            $query_usuario="SELECT * FROM `usuarios` WHERE NOMBRE_U='$nombre_usuario' ";
+            $verificar_usuario = mysqli_query($connection,$query_usuario);
+            $row_cont_U = $verificar_usuario->num_rows;
+            if($row_cont_U> 0){
+                echo '
+                    <script>
+                        alert("Este usuario ya fue registrado, intente con otro usuario");
+                        window.location = "registro.html";
+                    </script>
+                ';
+                exit();
+            }
+            
+            $ejecutar = mysqli_query($connection,$query);
+
+            if($ejecutar){
+                echo '
+                    <script>
+                        alert("Usuario creado exitosamente");
+                        window.location = "index.html";
+                    </script>
+                ';
+            }else{
+                echo '
+                    <script>
+                        alert("Intentelo de nuevo, usuario no creado");
+                        window.location = "registro.html";
+                    </script>
+                ';
+            }
         }else{
             echo '
-                <script>
-                    alert("Intentelo de nuevo, usuario no creado");
-                    window.location = "registro.html";
-                </script>
-            ';
+            <script>
+                alert("Contraseña insegura, La contraseña debe contener una mayuscula, minuscula y numero");
+                window.location = "registro.html";
+            </script>
+        ';
         }
     }else{
         echo '
@@ -65,5 +75,21 @@
     ';
     }
     msyqli_close($connection);
-    
+
+    function validar_clave($contrasena){
+        $res=true;
+        if(!preg_match('`[a-z]`',$contrasena)){
+            echo 'Clave debe tener una miniscula';
+            $res=false;
+        }
+        if(!preg_match('`[A-Z]`',$contrasena)){
+            echo 'Clave debe tener una mayuscula';
+            $res=false;
+        }
+        if(!preg_match('`[0-9]`',$contrasena)){
+            echo 'Clave debe tener una numero';
+            $res=false;
+        }
+        return $res;
+    }
 ?>
