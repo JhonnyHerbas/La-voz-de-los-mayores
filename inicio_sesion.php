41a99ad9a2ad9c44950f25654a7e0ae00217e68c
ViancaMamaniRamos <viancamamaniramos@gmail.com>
2022-05-05 13:44:00 -0400

    Bugs Solucionados Back

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -4,21 +4,120 @@
     $usuario= $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $query = "SELECT * FROM USUARIOS WHERE NOMBRE_U='$usuario' and CONTRASENIA_U='$contrasena' ";
-    $validar_inicio = mysqli_query($connection , $query);
+    $queryNombre = "SELECT * FROM USUARIOS WHERE NOMBRE_U='$usuario' ";
+    $queryContraseña=  "SELECT * FROM USUARIOS WHERE CONTRASENIA_U='$contrasena' ";
+    $validar_nombre = mysqli_query($connection , $queryNombre);
+    $validar_contraseña = mysqli_query($connection , $queryContraseña);
 
-    $row_cont = $validar_inicio->num_rows;
+    $row_contN = $validar_nombre->num_rows;
+    $row_contC = $validar_contraseña->num_rows;
 
-    if($row_cont >0){
-       header("location: index.html");
+    if($row_contN >0 and $row_contC>0){
+       header("location: musica.html");
         exit();
     }else{
-        echo '
-        <script>
-            alert("Por favor verifique los datos introducidos");
-            window.location = "IniciarSesion.html";
-        </script>
-    ';
-    exit();
+        if($row_contN >0 and $row_contC<1){
+            echo "<!DOCTYPE html>
+                <html lang='es'>
+                <head>
+                  <title>La Voz de los mayores</title>
+                  <meta charset='UTF-8'>
+                  <meta name='viewport' content='width=device-widtg, initiak-scale=1.0'>
+                
+                  <link href='css/style-ventana.css' rel='stylesheet'>
+                  <link rel='shortcut icon' href='logo1.png'>
+                </head>
+                <body>
+                  <header class='header'>
+                      <div class='container-superior'>
+                          <div>
+                              <a href='index.html'><img class='logo' src='logo1.png'> </a> 
+                              <h1 class='title'>  La Voz de los mayores</h1>   
+                          </div>
+                          <nav class='navigation'>
+                              <ul>
+                                  <li><a class='pagprinc' href='index.html'>Atrás</a></li>
+                              </ul>
+                          </nav>
+                          
+                      </div>
+                  </header>
+                  <main class='main'>
+                    <div class='container-medio'>
+                      <div class='ventana'>
+                          <h2 class='form-title'>Contraseña incorrecta, revise Por favor!</h2>
+                          <div class='block'>
+                          </div>
+                        
+                
+                      <div class='botones'>
+                          <a href='iniciarSesion.html'><button class='ok'>OK</button></a> 
+                
+                      </div>
+                  </div>        
+                </main>
+           </body>
+           
+           <footer>
+               <div class='container-inferior'>
+           
+               </div>
+           
+           </footer>
+           
+           </html>";
+           exit();
+        }else{
+            echo "<!DOCTYPE html>
+                <html lang='es'>
+                <head>
+                  <title>La Voz de los mayores</title>
+                  <meta charset='UTF-8'>
+                  <meta name='viewport' content='width=device-widtg, initiak-scale=1.0'>
+                
+                  <link href='css/style-ventana.css' rel='stylesheet'>
+                  <link rel='shortcut icon' href='logo1.png'>
+                </head>
+                <body>
+                  <header class='header'>
+                      <div class='container-superior'>
+                          <div>
+                              <a href='index.html'><img class='logo' src='logo1.png'> </a> 
+                              <h1 class='title'>  La Voz de los mayores</h1>   
+                          </div>
+                          <nav class='navigation'>
+                              <ul>
+                                  <li><a class='pagprinc' href='index.html'>Atrás</a></li>
+                              </ul>
+                          </nav>
+                          
+                      </div>
+                  </header>
+                  <main class='main'>
+                    <div class='container-medio'>
+                      <div class='ventana'>
+                          <h2 class='form-title'>El usuario que ingreso no existe. Intentelo de nuevo</h2>
+                          <div class='block'>
+                          </div>
+                        
+                
+                      <div class='botones'>
+                          <a href='iniciarSesion.html'><button class='ok'>OK</button></a> 
+                
+                      </div>
+                  </div>        
+                </main>
+           </body>
+           
+           <footer>
+               <div class='container-inferior'>
+           
+               </div>
+           
+           </footer>
+           
+           </html>";
+           exit();
+        }
     }
 ?>
