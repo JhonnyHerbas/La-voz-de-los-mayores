6fb92d40d07c9407228e80773821924cbd823177
jesicaV <96665613+jesicaV@users.noreply.github.com>
2022-05-17 19:48:31 -0400

    buscador

--- listaAudiolibro1.php
+++ listaAudiolibro1.php
@@ -3,7 +3,7 @@
 
     $buscar= $_POST['buscar'];
     if(!empty($buscar)){
-        $query = "select ID_AL,NOMBRE_AL,AUTOR_AL,CATEGORIA_AL from AUDIOLIBRO where NOMBRE_AL like '%".$buscar."%' ";
+        $query = "select ID_AL,NOMBRE_AL,AUTOR_AL,CATEGORIA_AL from AUDIOLIBRO where NOMBRE_AL like '%".$buscar."%' OR AUTOR_AL like '%" .$buscar."%' OR CATEGORIA_AL like '%".$buscar."%'";
         $result=mysqli_query($connection, $query);
         if(!$result){
             die('Quey Error'.mysqli_error($connection));
