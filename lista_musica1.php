3a77f2d7b0770a28a6cc1c2e25eb0aa06c38eb81
Darwin <darwingarcia2130@gmail.com>
2022-05-17 20:19:18 -0400

    buscador2.0

--- lista_musica1.php
+++ lista_musica1.php
@@ -2,14 +2,17 @@
     include('conexion.php');
 
     $buscar= $_POST['buscar'];
-    if(!empty($buscar)){
-        $query = "select ID_M,NOMBRE_M,AUTOR_M,CATEGORIA_M from MUSICA where NOMBRE_M like '%".$buscar."%' ";
+    $json=array();
+    
+
+    if(empty($buscar)){
+
+        $query = "select ID_M,NOMBRE_M,AUTOR_M,CATEGORIA_M from MUSICA ";
         $result=mysqli_query($connection, $query);
         if(!$result){
             die('Quey Error'.mysqli_error($connection));
         
-        }
-        $json=array();
+        }      
         while($row = mysqli_fetch_array($result)){
             $json[]=array(
                 'ID_M' => $row['ID_M'],
@@ -20,5 +23,29 @@
         }
         $jsonstring=json_encode($json);
         echo $jsonstring;
+
+
+        
+    }else{
+       
+        $query = "select ID_M,NOMBRE_M,AUTOR_M,CATEGORIA_M from MUSICA where NOMBRE_M like '%".$buscar."%' 
+        OR AUTOR_M like '%".$buscar."%' OR CATEGORIA_M like '%".$buscar."%'";
+        $result=mysqli_query($connection, $query);
+        if(!$result){
+            die('Quey Error'.mysqli_error($connection));
+        
+        }      
+        while($row = mysqli_fetch_array($result)){
+            $json[]=array(
+                'ID_M' => $row['ID_M'],
+                 'NOMBRE_M' => $row['NOMBRE_M'],
+                'AUTOR_M' => $row['AUTOR_M'],
+                'CATEGORIA_M' => $row['CATEGORIA_M']
+            );
+        }
+        $jsonstring=json_encode($json);
+        echo $jsonstring;
+
     }
+    
 ?>
